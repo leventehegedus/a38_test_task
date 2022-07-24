e07dd0ef08c541e79e04fb8bb7571f637ec2148b
leventehegedus <levi.hegedus92@gmail.com>
2022-07-24 23:25:18 +0200

    removed unnecessary packages, import, readme.md added

--- a38-app/src/App.tsx
+++ a38-app/src/App.tsx
@@ -1,18 +1,16 @@
 import './App.scss';
 import Wizard from './components/Wizard/Wizard';
-import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './styles/theme';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <div className="App">
         <Wizard />
       </div>
-      <ToastContainer autoClose={2000} />
-    </ThemeProvider>
+      <ToastContainer autoClose={1000} />
+    </>
   );
 }
 
